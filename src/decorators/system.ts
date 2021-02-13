242a5d59ceaa8df673f61e46026184a5bd32a60e
Felipe Orellana <forellana@digithinkit.com>
2021-02-13 13:51:09 -0500

    chore: rename execgroup to pipelines

--- src/decorators/system.ts
+++ src/decorators/system.ts
@@ -5,7 +5,7 @@ import { IFilter, ISystem } from "../data/types";
 export function makeSystem(system: ISystem, filter: IFilter, ecsName: string = "default", executionGroup: string = EXEC_PER_FRAME) {
   const ecs = globalECSCache.get(ecsName);
   if ( ecs ) {
-    ecs.execGroup.children.get(executionGroup)?.systems.add(system, filter);
+    ecs.pipeline.children.get(executionGroup)?.systems.add(system, filter);
   } else {
     throw new Error(`ECS does not exist: ${ecsName}`);
   }
