4b4603745d750dc02b28179f85d325abfb1fa9f6
Felipe Orellana <forellana@digithinkit.com>
2021-05-02 11:38:26 -0400

    feat: type adjustments to ease cli tool setup

--- src/ecs_base.ts
+++ src/ecs_base.ts
@@ -2,31 +2,33 @@ import { EXEC_PER_FRAME } from './contants';
 import { ComponentManager } from './data/componentManager';
 import { EntityCollection } from './data/entityCollection';
 import { Pipeline } from './data/pipeline';
-import { IComponentSchema, IECSSchema } from './schemas/types';
-import { ECSOpts, IComponentDefinition } from './types';
+import { IComponents, IComponentSchema, IECSSchema } from './schemas/types';
+import { ECSOpts, IDefinitions } from './types';
 export * from './schemas/types';
 export * from './data/entity';
 
 /**
  * Manages multiple ECS Instances
  */
-export const globalECSCache = new Map<string, ECSBase<any>>();
+export const globalECSCache = new Map<string, ECSBase<any, any>>();
+
+
 
 /**
  * Type safe ECS system entry point.
  */
-export class ECSBase<C extends IComponentDefinition> {
-  public readonly entities: EntityCollection<C>;
-  public readonly pipeline: Pipeline<C>;
-  public readonly componentManager: ComponentManager<C>;
+export class ECSBase<D extends IDefinitions, C extends IComponents> {
+  public readonly entities: EntityCollection<D, C>;
+  public readonly pipeline: Pipeline<D, C>;
+  public readonly componentManager: ComponentManager<D, C>;
 
-  constructor(public readonly opts: ECSOpts) {
-    this.pipeline = new Pipeline<C>(this);
-    this.entities = new EntityCollection<C>(this);
-    this.componentManager = new ComponentManager<C>(this);
+  constructor(public readonly opts: ECSOpts<C>) {
+    this.pipeline = new Pipeline<D, C>(this);
+    this.entities = new EntityCollection<D, C>(this);
+    this.componentManager = new ComponentManager<D, C>(this);
 
     // Default pipeline
-    this.pipeline.children.set(EXEC_PER_FRAME, new Pipeline<C>(this));
+    this.pipeline.children.set(EXEC_PER_FRAME, new Pipeline<D, C>(this));
     globalECSCache.set(this.name, this);
   }
 
@@ -40,7 +42,7 @@ export class ECSBase<C extends IComponentDefinition> {
   /**
    * ECS Component/Entity Schema
    */
-  get schema(): IECSSchema {
+  get schema(): IECSSchema<C> {
     return this.opts.schema;
   }
 
@@ -48,7 +50,7 @@ export class ECSBase<C extends IComponentDefinition> {
    * Collection of systems on the default pipeline
    */
   get systems() {
-    const perFrame = this.pipeline.children.get(EXEC_PER_FRAME) as Pipeline<C>;
+    const perFrame = this.pipeline.children.get(EXEC_PER_FRAME) as Pipeline<D, C>;
     return perFrame?.systems;
   }
 
